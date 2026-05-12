50ee4a39aa4c28dcbe48a5343e140e7022fdec3b
Dan Allan <dallan@bnl.gov>
2026-05-12 07:37:37 -0400

    Add upload code to serve

--- serve.py
+++ serve.py
@@ -15,11 +15,63 @@ Extra uvicorn options are passed through directly:
 """
 
 import argparse
+import collections
 import os
 import sys
+import json
+import traceback
+import sys
+from functools import partial
+
+import httpx
+from tiled.client import from_uri
 
 
-responses = []
+responses = collections.deque()
+
+
+def build_body(update, tiled_uri):
+    metadata = update.metadata
+    if update.structure_family == "container":
+        body = { 
+            "type": "artifactCollection",
+            "name": update.key,
+            "description": metadata.get("description", json.dumps(metadata)),
+            "display_name": metadata.get("display_name", metadata["uid"]),
+            "location": f"{tiled_uri}/{update.key}",
+            "parent_fqn": "bnl-lightshow-storage.bnl-lightshow-catalog.base",
+        }
+    else:
+        body = { 
+            "type": "artifact",
+            "name": update.key,
+            "description": metadata.get("description", json.dumps(metadata)),
+            "display_name": metadata.get("display_name", metadata["uid"]),
+            "location": f"{tiled_uri}/{update.key}",
+            "parent_fqn": "bnl-lightshow-storage.bnl-lightshow-catalog.base",
+            "format": update.data_sources[0].mimetype,
+            # "size":  # add this when assets know their size
+        }
+    return body
+
+
+def upload(update, tiled_uri, client):
+    try:
+        body = build_body(update, tiled_uri)
+        entity_type = body["type"]
+        catalog_name = os.environ["AMSC_OPENMETADATA_CATALOG_NAME"]
+        response = client.post(
+            f"/catalog/{catalog_name}/{entity_type}",
+            headers={
+                "Authorization": f"Bearer {os.environ['AMSC_OPENMETADATA_TOKEN']}",
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json=body,
+        )
+        response.raise_for_status()
+    except Exception as exc:
+        traceback.print_exc() 
 
 
 def main() -> None:
